4903c996ebea494c00edf996fb5cda52f3af21c4
Elvis Paez <elvispaez18@gmail.com>
2026-08-11 13:20:51 -0400

    [MIG] migration script

--- account_ux/migrations/18.0.1.0.0/pre-migration.py
+++ account_ux/migrations/18.0.1.0.0/pre-migration.py
@@ -7,6 +7,17 @@ def migrate(cr, version):
     adding in the previous commit
     """
     env = api.Environment(cr, SUPERUSER_ID, {})
+
+    # This inherited view was removed from the module in 16.0, but upgrades
+    # keep the database record. It still targets the old currency_rate field
+    # and prevents the new Odoo 18 base view from being validated.
+    obsolete_view = env.ref(
+        "account_ux.view_account_change_no_exchange_currency",
+        raise_if_not_found=False,
+    )
+    if obsolete_view:
+        obsolete_view.unlink()
+
     view = env.ref("account_ux.view_account_form", raise_if_not_found=False)
     if view:
         view.unlink()
